4fc379c14b14330c2399ec6fa7e8c00f3bafc986
lenove <271130940@qq.com>
2016-08-07 21:10:22 +0800

    none

--- Assets/Scripts/Promotion/CatalogueAllBtnCtr.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class CatalogueAllBtnCtr : MonoBehaviour {
-    public CatalogueGroupControl[] CataGroupControlScs;  //  所有目录模板的按钮管理脚本
-	// Use this for initialization
-}
